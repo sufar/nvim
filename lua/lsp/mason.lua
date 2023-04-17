afe42be70d7b70253f24d5d9d88dfc2460fcc26c
sufar <fozoto@163.com>
2023-04-17 13:09:08 +0800

    fix cssls

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,7 +1,4 @@
-require("neodev").setup({
-  -- add any options here, or leave empty to use the default settings
-})
-
+require "lsp.lua"
 
 require("mason").setup()
 require("mason-lspconfig").setup()
@@ -11,7 +8,7 @@ require("mason-lspconfig").setup_handlers {
   function(server_name)
     require("lspconfig")[server_name].setup {}
   end,
-  ["jdtls"] = function()
+      ["jdtls"] = function()
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "java" },
       callback = function()
@@ -21,23 +18,45 @@ require("mason-lspconfig").setup_handlers {
       group = vim.api.nvim_create_augroup("jdtls", { clear = true })
     })
   end,
-  ["sqls"] = function()
+      ["sqls"] = function()
     require('lspconfig').sqls.setup {
       on_attach = function(client, bufnr)
         require('sqls').on_attach(client, bufnr)
       end
     }
   end,
-  ["rust_analyzer"] = function()
+      ["rust_analyzer"] = function()
     local config = require("lsp.rust")
     require("rust-tools").setup(config)
     require('crates').setup()
   end,
-  ["groovyls"] = function()
+      ["groovyls"] = function()
     require 'lspconfig'.groovyls.setup {
       cmd = { "groovy-language-server" },
       filetypes = { "groovy" },
       root_dir = require('lspconfig').util.root_pattern("*.groovy")
+    }
+  end,
+      ["cssls"] = function()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    require 'lspconfig'.cssls.setup {
+      capabilities = capabilities,
+      cmd = { mason_home .. "/bin/vscode-css-language-server", "--stdio" },
+      filetypes = { "css", "scss", "less" },
+      root_dir = require('lspconfig').util.root_pattern("package.json", ".git") or bufdir,
+      settings = {
+        css = {
+          validate = true
+        },
+        less = {
+          validate = true
+        },
+        scss = {
+          validate = true
+        }
+      },
+      single_file_support = true,
     }
   end
 }
--- lua/tools/neodev.lua
+++
@@ -1,8 +0,0 @@
-require("neodev").setup()
-vim.lsp.start({
-  name = "lua-language-server",
-  cmd = { "lua-language-server" },
-  before_init = require("neodev.lsp").before_init,
-  root_dir = vim.fn.getcwd(),
-  settings = { Lua = {} },
-})
